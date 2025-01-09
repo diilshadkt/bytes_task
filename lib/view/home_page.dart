1be609eee4f80c833bce8c441d554a8cbe51606a
diilshadkt <dilshadktkunnathodi@gmail.com>
2025-01-09 12:29:11 +0530

    home page UI completed

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:bytes_task/view/overview_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +6,69 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 234, 234, 234),
+      appBar: AppBar(
+        title: Text(
+          "Bytes",
+          style: TextStyle(
+              fontSize: 27,
+              fontWeight: FontWeight.bold,
+              color: Colors.blueAccent),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  "Choose your Product",
+                  // textAlign: TextAlign.end,
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisSpacing: 15,
+                    mainAxisSpacing: 15,
+                    crossAxisCount: 2),
+                itemCount: 9,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => OverviewPage(),
+                          ));
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Center(
+                        child: Icon(Icons.person),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/view/overview_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OverviewPage extends StatelessWidget {
+  const OverviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 234, 234, 234),
+    );
+  }
+}
